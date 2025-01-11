be3f8e519380f215b529275873e122477000c9b3
Jaren <jhan94@claytonschools.net>
2025-01-11 13:44:34 -0600

    we documented the levels

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,3 +1,137 @@
 package frc.robot.subsystems.elevator;
 
-public class Elevator {}
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.hardware.Motor;
+import frc.robot.hardware.Motor.TargetType;
+import frc.robot.utilities.FeedbackController;
+
+public class Elevator extends SubsystemBase {
+  private Motor upMotor;
+
+  public Elevator() {
+    upMotor =
+        Motor.fromIdealSim(
+            FeedbackController.fromPID(
+                new PIDController(10, 0, 0),
+                pid -> {
+                  pid.setTolerance(0.01);
+                }),
+            TargetType.Position,
+            0);
+  }
+
+  /**
+   * @return A command that stows the elevator
+   */
+  public Command stow() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(0);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+  /**
+   * 
+   * @return A command that makes the elevator go up to the fourth level
+   */
+  public Command level4() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(2);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+    /**
+    * @return A command that makes the elevator go to the third level
+     */
+  public Command level3() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(1.5);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+
+  /**
+   * @return A command that moves the elevator up to the second level
+   */
+  public Command level2() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(1);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil( 
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+  
+  /**
+   * 
+   * @return A command that moves the elevator up to the first level
+   */
+  public Command level1() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(0.5);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+
+  /**
+   * 
+   * @return A command that moves the elevator to the level of the lower algae
+   */
+  public Command lowAlgae() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(.75);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+/**
+ * @return A command that moves the elevator to the level of the higher algae
+ */
+  public Command highAlgae() {
+    return Commands.runOnce(
+            () -> {
+              upMotor.setTarget(1.75);
+            },
+            this)
+        .andThen(
+            Commands.waitUntil(
+                () -> {
+                  return upMotor.atTarget();
+                }));
+  }
+}
