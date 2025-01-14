277957559f56624df481bff7a0f62373d70cac51
Alana <lonivic06@gmail.com>
2025-01-14 16:59:14 -0600

    grabber slaaber
     also i'm not sure why there is a runOnce error

---
+++ src/main/java/frc/robot/subsystems/grabber/Grabber.java
@@ -0,0 +1,88 @@
+package frc.robot.subsystems.grabber;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.hardware.Motor;
+import frc.robot.hardware.Motor.TargetType;
+import frc.robot.utilities.FeedbackController;
+import frc.robot.utilities.logging.HoundLog;
+import frc.robot.utilities.logging.Loggable;
+
+
+
+
+public class Grabber {
+    
+    private Motor speedstr;
+
+    public Grabber() {
+      speedstr =
+          Motor.fromIdealSim(
+              FeedbackController.fromPID(
+                  new PIDController(10, 0, 0),
+                  pid -> {
+                    pid.setTolerance(0.01);
+                  }),
+              TargetType.Position,
+              0);
+    }
+  
+    /**
+     * @return A command that stowps the grabber
+     */
+    public Command stop() {
+      return Commands.runOnce(
+              () -> {
+                speedstr.setTarget(0.5);
+              },
+              this)
+          .andThen(
+              Commands.waitUntil(
+                  () -> {
+                    return speedstr.atTarget();
+                  }));
+    }
+  
+    /**
+     * @return A command that makes the intaker suck
+     */
+    public Command intake() {
+      return Commands.runOnce(
+              () -> {
+                speedstr.setTarget(2);
+              },
+              this)
+          .andThen(
+              Commands.waitUntil(
+                  () -> {
+                    return speedstr.atTarget();
+                  }));
+    }
+  /**
+     * @return A command that makes the shooter?? intaker?? spit
+     */
+    public Command eject() {
+        return Commands.runOnce(
+                () -> {
+                    speedstr.setTarget(2);
+                },
+                this)
+            .andThen(
+                Commands.waitUntil(
+                    () -> {
+                      return speedstr.atTarget();
+                    }));
+      }
+    
+  
+
+      public void log(String path) {
+        HoundLog.log(path, "Speed Motor", speedstr);
+      }
+
+
+
+
+}
