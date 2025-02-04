8072961c33eb4d98a8cf17b93d485c34ee29c70c
Vimal Buckley <vbuckley68@claytonschools.net>
2025-02-03 20:10:25 -0600

    temp commit

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -343,15 +343,14 @@ public class Swerve extends SubsystemBase implements Loggable {
   public Command testDriveConversionFactor(double speed, double duration) {
     return Commands.run(
       () -> {
-        drive(new ChassisSpeeds(0, 0, speed));
-        HoundLog.log("Swerve/Characterization/Gyro Speed", gyro.getAngularVelocity().getRadians());
-        HoundLog.log("Swerve/Characterization/Odometry Speed", getSpeeds().omegaRadiansPerSecond);
-        HoundLog.log("Swerve/Characterization/FL Speed", modules[0].getCurrentState().speedMetersPerSecond / FRONT_LEFT_TRANSLATION.getNorm());
-        HoundLog.log("Swerve/Characterization/FR Speed", modules[1].getCurrentState().speedMetersPerSecond / FRONT_RIGHT_TRANSLATION.getNorm());
-        HoundLog.log("Swerve/Characterization/BL Speed", modules[2].getCurrentState().speedMetersPerSecond / BACK_LEFT_TRANSLATION.getNorm());
-        HoundLog.log("Swerve/Characterization/BR Speed", modules[3].getCurrentState().speedMetersPerSecond / BACK_RIGHT_TRANSLATION.getNorm());
-      }, this
-    ).withTimeout(duration);
+              drive(new ChassisSpeeds(0, 0, speed));
+              HoundLog.log(
+                  "Swerve/Characterization/Gyro Speed", gyro.getAngularVelocity().getRadians());
+              HoundLog.log(
+                  "Swerve/Characterization/Odometry Speed", getSpeeds().omegaRadiansPerSecond);
+            },
+            this)
+        .withTimeout(duration);
   }
 
   private ChassisSpeeds calculateVelRobotRel(XboxController xbox) {
